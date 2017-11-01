140065f89474977f4a82dce588346bcb08217871
Azerpas <xskylix@hotmail.fr>
2017-11-01 23:03:13 +0100

    1.1: Proxy & Country support

--- NEC.py
+++ NEC.py
@@ -15,11 +15,13 @@ class NEC(object):
 		self.events = [] # {'date':'','name':''}
 		self.s = requests.session()
 		self.url = "https://www.nike.com/events-registration/event?id="
-		self.start = 90174
+		self.start = 90174 #91472
 		self.country = ""
+		self.proxy = {'http':'','https':'','ftp':''}
+		self.headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 	def check(self,ad):
-		req = self.s.get(self.url+str(ad)).text
+		req = self.s.get(self.url+str(ad), headers = self.headers, proxies = self.proxy).text
 		soup = BeautifulSoup.BeautifulSoup(req)
 		script = soup.findAll('script')
 		pattern = re.compile("""eventDetails":(.*),"languageLocales""") #nike.events.content = {(.*)
@@ -27,14 +29,17 @@ class NEC(object):
 		try:
 			m = json.loads(fields[0])
 		except:
+			theid = colored(str(ad),'blue')
 			print("!!!!!!!!!!!!!!!!!!!!!!")
-			print("ERROR: CAN'T READ DATA OF ID: "+str(ad))
+			print("ERROR: CAN'T READ DATA OF ID: "+theid)
 			print("!!!!!!!!!!!!!!!!!!!!!!")
 			return
 		log("Data retrieved")
+		print(m[0]['language']['countryCode'])
 		if self.country != "":
 			if m[0]['language']['countryCode'] != self.country:
 				return
+
 		name = colored(m[0]['name'], 'red', attrs=['bold','underline'])
 		country = colored(m[0]['language']['countryCode'], 'blue')
 		description = colored(m[0]['description'].replace("<br>", "").replace("<br />","").replace("<br/>","").replace("&amp;","&") , 'yellow')
@@ -49,8 +54,7 @@ class NEC(object):
 		log("Starting")
 		print(colored("\nDo you want to search for events of a specific country?",'red'))
 		aux = raw_input("Press fr, us, gb, etc... or nothing to continue: ")
-		# you can add country id in here if you find some
-		if aux in ['fr','us','tr','tw','la','gb','ru','pl','gr','ar','es','th','it','pt']:
+		if aux in ['fr','us','tr','tw','la','gb','ru','pl','gr','ar','es','th','it','pt','nl','ar']:
 			self.country = aux
 		else:
 			self.country = ""
