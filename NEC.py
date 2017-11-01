ff10ce593791898f2d67d3af7da6da60657129c7
Azerpas <xskylix@hotmail.fr>
2017-11-02 00:06:28 +0100

    1.1, added Pickle support, save events to a .txt

--- NEC.py
+++ NEC.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-import requests, BeautifulSoup, time, datetime, re, json, random
+import requests, BeautifulSoup, time, datetime, re, json, random, pickle
 from termcolor import colored
 
 
@@ -12,10 +12,10 @@ def log(event):
 
 class NEC(object):
 	def __init__(self):
-		self.events = [] # {'date':'','name':''}
+		self.events = {} # {'date':'','name':''}
 		self.s = requests.session()
 		self.url = "https://www.nike.com/events-registration/event?id="
-		self.start = 90174 #91472
+		self.start = 91472 #91472
 		self.country = ""
 		self.proxy = {'http':'','https':'','ftp':''}
 		self.headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
@@ -31,39 +31,69 @@ class NEC(object):
 		except:
 			theid = colored(str(ad),'blue')
 			print("!!!!!!!!!!!!!!!!!!!!!!")
-			print("ERROR: CAN'T READ DATA OF ID: "+theid)
+			print("ERROR: CAN'T READ DATA OF ID: " + theid)
 			print("!!!!!!!!!!!!!!!!!!!!!!")
 			return
 		log("Data retrieved")
-		print(m[0]['language']['countryCode'])
 		if self.country != "":
 			if m[0]['language']['countryCode'] != self.country:
 				return
 
 		name = colored(m[0]['name'], 'red', attrs=['bold','underline'])
 		country = colored(m[0]['language']['countryCode'], 'blue')
-		description = colored(m[0]['description'].replace("<br>", "").replace("<br />","").replace("<br/>","").replace("&amp;","&") , 'yellow')
+		description = colored(m[0]['description'].replace("<br>", "").replace("<br />","").replace("<br/>","").replace("&amp;","&") , 'yellow').replace("</b>","").replace("<b>","")
 		url = colored(m[0]['eventUrl'], 'blue') 
 		print("-------------------------")
 		print("||" + name + "||" + country)
 		print("||" + description + "||")
 		print("||" + url + "||")
 		print("-------------------------")
+		self.events = {'name':name,'description':description,'url':url,'country':country,'id':ad}
+		self.save()
+
+	def save(self):
+		log("Saving event into 'event.txt'")
+		try:
+			file = open('event.txt','r')
+			print('TXT already exists')
+			try:
+				Filelist = pickle.load(file)
+			except EOFError:
+				print("--------------------")
+				print("Event error")
+				print("--------------------")
+				return
+			Filelist.append(self.events)
+			file = open('event.txt','w')
+			pickle.dump(Filelist,file)
+
+		except IOError as e:
+			print('Creating txt')
+			file = open('event.txt','w')
+			Filelist = []
+			Filelist.append(self.events)
+			#file.write(Filelist)
+			pickle.dump(Filelist,file)
+			#file.write('\n')
+	print('Event successfuly saved')
+
+
 
 	def main(self):
 		log("Starting")
 		print(colored("\nDo you want to search for events of a specific country?",'red'))
-		aux = raw_input("Press fr, us, gb, etc... or nothing to continue: ")
+		aux = raw_input("Press fr, us, gb, etc... or return to continue: ")
 		if aux in ['fr','us','tr','tw','la','gb','ru','pl','gr','ar','es','th','it','pt','nl','ar']:
 			self.country = aux
 		else:
 			self.country = ""
 		# fr us tr tw la gb ru pl gr ar es th
 		while True:
-			log("Scraping data")
+			log("Scraping data of ID: " + str(self.start))
 			self.check(self.start)
-			time.sleep(random.uniform(2.3,2.6))
+			time.sleep(random.uniform(0.3,0.6))
 			self.start += 1
+			print("\n")
 
 
 
