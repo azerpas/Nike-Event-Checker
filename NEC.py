92b89335d308320e928db4bbe230ce4a4c3a4647
Azerpas <xskylix@hotmail.fr>
2017-11-01 22:17:24 +0100

    Update NEC.py

--- NEC.py
+++ NEC.py
@@ -16,6 +16,7 @@ class NEC(object):
 		self.s = requests.session()
 		self.url = "https://www.nike.com/events-registration/event?id="
 		self.start = 90174
+		self.country = ""
 
 	def check(self,ad):
 		req = self.s.get(self.url+str(ad)).text
@@ -27,21 +28,33 @@ class NEC(object):
 			m = json.loads(fields[0])
 		except:
 			print("!!!!!!!!!!!!!!!!!!!!!!")
-			print("ERROR: CAN'T READ DATA OF ID: ")
+			print("ERROR: CAN'T READ DATA OF ID: "+str(ad))
 			print("!!!!!!!!!!!!!!!!!!!!!!")
 			return
 		log("Data retrieved")
+		if self.country != "":
+			if m[0]['language']['countryCode'] != self.country:
+				return
 		name = colored(m[0]['name'], 'red', attrs=['bold','underline'])
+		country = colored(m[0]['language']['countryCode'], 'blue')
 		description = colored(m[0]['description'].replace("<br>", "").replace("<br />","").replace("<br/>","").replace("&amp;","&") , 'yellow')
 		url = colored(m[0]['eventUrl'], 'blue') 
 		print("-------------------------")
-		print("||" + name + "||")
+		print("||" + name + "||" + country)
 		print("||" + description + "||")
 		print("||" + url + "||")
 		print("-------------------------")
 
 	def main(self):
 		log("Starting")
+		print(colored("\nDo you want to search for events of a specific country?",'red'))
+		aux = raw_input("Press fr, us, gb, etc... or nothing to continue: ")
+		# you can add country id in here if you find some
+		if aux in ['fr','us','tr','tw','la','gb','ru','pl','gr','ar','es','th','it','pt']:
+			self.country = aux
+		else:
+			self.country = ""
+		# fr us tr tw la gb ru pl gr ar es th
 		while True:
 			log("Scraping data")
 			self.check(self.start)
