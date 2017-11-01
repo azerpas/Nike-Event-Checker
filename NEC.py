0d9dbccaf4673a6d5af091a5a36630e4b7536739
Azerpas <xskylix@hotmail.fr>
2017-11-01 21:15:34 +0100

    1.0 release
    Working good, but too many events, need a filter

--- NEC.py
+++ NEC.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
-import requests, BeautifulSoup, pickle, time, datetime, re, json
+import requests, BeautifulSoup, time, datetime, re, json, random
+from termcolor import colored
 
 
 d = datetime.datetime.now().strftime('%H:%M:%S')
@@ -14,20 +15,40 @@ class NEC(object):
 		self.events = [] # {'date':'','name':''}
 		self.s = requests.session()
 		self.url = "https://www.nike.com/events-registration/event?id="
-		self.start = "91597"
+		self.start = 90174
 
-	def check(self):
-		req = self.s.get(self.url+self.start).text
+	def check(self,ad):
+		req = self.s.get(self.url+str(ad)).text
 		soup = BeautifulSoup.BeautifulSoup(req)
 		script = soup.findAll('script')
 		pattern = re.compile("""eventDetails":(.*),"languageLocales""") #nike.events.content = {(.*)
 		fields = re.findall(pattern, script[4].text)
-		
-		m = json.loads(fields[0])
-		print(m)
+		try:
+			m = json.loads(fields[0])
+		except:
+			print("!!!!!!!!!!!!!!!!!!!!!!")
+			print("ERROR: CAN'T READ DATA OF ID: ")
+			print("!!!!!!!!!!!!!!!!!!!!!!")
+			return
+		log("Data retrieved")
+		name = colored(m[0]['name'], 'red', attrs=['bold','underline'])
+		description = colored(m[0]['description'].replace("<br>", "").replace("<br />","").replace("<br/>","").replace("&amp;","&") , 'yellow')
+		url = colored(m[0]['eventUrl'], 'blue') 
+		print("-------------------------")
+		print("||" + name + "||")
+		print("||" + description + "||")
+		print("||" + url + "||")
+		print("-------------------------")
 
 	def main(self):
-		self.check()
+		log("Starting")
+		while True:
+			log("Scraping data")
+			self.check(self.start)
+			time.sleep(random.uniform(2.3,2.6))
+			self.start += 1
+
+
 
 if __name__ == "__main__":
 	NEC = NEC()
